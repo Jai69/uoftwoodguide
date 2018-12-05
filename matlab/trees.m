4b5b11f1b9727adcf6f496e23b501405d965ac18
Shane Guignard <shaneguignard@gmail.com>
2018-12-05 15:22:00 -0500

    major UI and js update

--- matlab/trees.m
+++ matlab/trees.m
@@ -1,17 +1,172 @@
-% University of Toronto Wood Collection
+db = readtable('treeDatabase.csv')
 
-% Download database file
-db = readtable('database.txt') % This produced and error "unable to open file 'database.txt'
+db.Date = datetime(db.Date, 'ConvertFrom', double) 
+% Connot convert from double to datetime.
 
-% Luckly I had the original .xlsx file
-db = readtable('documents/uoftCatClean.xlsx') 
+db.Date = datetime(string(db.Date), 'InputFormat', 'yyyy') 
+% By converting the date column to a string I was able to convert it to a DateTime
 
-% Wanted to confirm if the date column was a datetime type or cell?
-class(db.Date)  % returns cell
+unique(db.Collector) % Checks for unique Collector
+% Returns list of 53 unique contributor with some spelling errors which
+% generate duplicates
+
+db(db.Collector == 'Comercial', 'Collector') % Cannot use == with cell
+
+% convert 'Collectors' to string
+db.Collector = string(db.Collector)
+
+% Try to correct spelling mistake
+db(db.Collector == 'Comercial', 'Collector') = "Commercial" % must be table or array
+
+db(db.Collector == 'Prof. e. Fritz', 'Collector') = {'Prof. E. Fritz'} % requires that you define the variable as a cell
+db(db.Collector == 'E. Fritz', 'Collector') = {'Prof. E. Fritz'}
+db(db.Collector == 'E.J.Z', 'Collector') = {'E.J. Zavitz'}
+db(db.Collector == 'E.J.Z.', 'Collector') = {'E.J. Zavitz'}
+db(db.Collector == 'J.H.W.', 'Collector') = {'J.H. White'}
+db(db.Collector == 'J.H.White', 'Collector') = {'J.H. White'}
+db(db.Collector == 'Professor. Fritz', 'Collector') = {'Prof. E. Fritz'}
+db(db.Collector == 'Comercial', 'Collector') = {'Commercial'}
+% Correct all spelling mistakes for Collector identified by unique
+
+% Double Check that there are no duplicates.
+unique(db.Collector)
 
 
-db.Date = datetime(db.Date, 'InputFormat', '%{yyyy}D') 
+% I noticed that there was some white spaces before and after some of the
+% entries in the Contributors column, so I was able to clean that up using
+% the strtirm() function
+db.Contributor = strtrim(db.Contributor)
 
-class(db.Date)
+unique(db.Contributor) % checks to see if there are any duplicate issues with the names
+% This returns a list of 37 unique Contributors, with some spelling errors which generate duplicates. 
+% with some of whom are the names of collectors, these names will need to
+% be moved to the collectors column.
 
-dates = db.Date
+db.Contributor = string(db.Contributor)
+%Convert db.Contributor to string
+
+db(db.Contributor == 'Botanical station', 'Contributor') = {'Botanical Station'};
+db(db.Contributor == 'Department of lands and mines', 'Contributor') = {'Department of Lands and Mines'};
+db(db.Contributor == 'Forest Service', 'Contributor') = {'Forest Services'};
+% Correct all the duplicate entries
+
+%Double Check to makes sure the correction worked
+unique(db.Contributor)
+
+% check to see if there are any doubles where the contributor and collector
+% are the same. This is to try and see if there are names of the collectors
+% in the contributor column.
+db(db.Contributor == db.Collector & db.Contributor ~= '', :)
+
+db(db.Contributor == 'E.J.Z.' & db.Collector == '', 'Collector') = {'E.J. Zavitz'}
+db(db.Contributor == 'J.H. White' & db.Collector == '', 'Collector') = {'J.H. White'}
+db(db.Contributor == 'J.R. Cosgrove' & db.Collector == '', 'Collector') = {'J.R. Cosgrove'}
+db(db.Contributor == 'R.G. Chesbro' & db.Collector == '', 'Collector') = {'R.G. Chesbro'}
+
+db(db.Contributor == 'E.J.Z.', 'Contributor') = {''}
+db(db.Contributor == 'J.H. White', 'Contributor') = {''}
+db(db.Contributor == 'J.R. Cosgrove', 'Contributor') = {''}
+db(db.Contributor == 'R.G. Chesbro', 'Contributor') = {''}
+
+% Double Check to make sure that the contributors are all unique now
+unique(db.Contributor)
+
+%Capitalize all the first letters of the Genus
+unique(db.Genus)
+db.Genus = char(db.Genus)
+db.Genus(:,1:1) = upper(db.Genus(:,1:1))
+db.Genus = string(db.Genus)
+db.Genus = strtrim(db.Genus) 
+unique(db.Genus)
+
+% Call function to capitalize the first letters of all species caps.m
+db.Species = caps(db.Species)
+
+% Double check that all Species are unique and capitalized
+unique(db.Species) 
+
+% Capitalize first letter of all Families
+
+db.Family = caps(db.Family)
+unique(db.Family) % Double check that all names were capitalized and are unique.
+
+% Capitalize first letter all ComName
+db.ComName = caps(db.ComName)
+unique(db.ComName) % Double check that all names were capitalized and are unique.
+
+% Capitalize all Location
+db.Location = caps(db.Location)
+unique(db.Location)
+
+% Apply a number of corrections to the db.Location column 
+db(db.Location == 'Austrailia', 'Location') = {'Australia'}
+db(db.Location == 'Britiah Guiana', 'Location') = {'British Guiana'}
+db(db.Location == 'Burma', 'Location') = {'Burma, India'}
+db(db.Location == 'Caeylon', 'Location') = {'Ceylon, Sk, Canada'}
+db(db.Location == 'Ceylon', 'Location') = {'Ceylon, Sk, Canada'}
+db(db.Location == 'Fiji Ids.', 'Location') = {'Fiji'}
+db(db.Location == 'From Cotswold Hills, Gloucestershire', 'Location') = {'Cotswold Hills, Gloucestershire'}
+db(db.Location == 'From Forest of Dean, Gloucestershire', 'Location') = {'Forest of Dean, Gloucestershire'}
+db(db.Location == 'From Gloucestershire', 'Location') = {'Gloucestershire'}
+db(db.Location == 'From tree grown in Gloucester city', 'Location') = {'Gloucester City'}
+db(db.Location == 'Interior mill, B.C.', 'Location') = {'Interior, B.C.'}
+db(db.Location == 'J.H.White', 'Collector') = {'J.H. White'}
+db(db.Location == 'J.H.White', 'Location') = {''}
+db(db.Location == 'J.R. Cosgrove', 'Collector') = {'J.R. Cosgrove'}
+db(db.Location == 'J.R. Cosgrove', 'Location') = {''}
+db(db.Location == 'Malay', 'Location') = {'Malay States'}
+db(db.Location == 'QUeensland', 'Location') = {'Queensland'}
+db(db.Location == 'S.J. Record', 'Collector') = {'S.J. Record'}
+db(db.Location == 'S.J. Record', 'Location') = {''}
+db(db.Location == 'St Williams, Ont.', 'Location') = {'St. Williams, Ont.'}
+db(db.Location == 'St. Williams', 'Location') = {'St. Williams, Ont.'}
+db(db.Location == 'Strts. Settlement', 'Location') = {'Atrts. Settlements'}
+db(db.Location == 'Strts. Settlements', 'Location') = {'Atrts. Settlements'}
+db(db.Location == 'Ststs. Settlements', 'Location') = {'Atrts. Settlements'}
+db(db.Location == 'Toronto', 'Location') = {'Toronto, Ontario'}
+db(db.Location == 'Vancouver', 'Location') = {'Vancouver, B.C.'}
+db(db.Location == 'Wycliffe, B.C', 'Location') = {'Wycliffe, B.C.'}
+
+% Verify that the corrections were applied
+unique(db.Location)
+
+% If the Genus, Species, Family, or ComName are blank, change to unknown
+db(db.Genus == '', 'Genus') = {'Unknown'}
+db(db.Species == '', 'Species') = {'Unknown'}
+db(db.Family == '', 'Family') = {'Unknown'}
+db(db.ComName == '', 'ComName') = {'Unknown'}
+
+% Convert to string
+db.Family = string(db.Family);
+% Apply a number of corrections to the tree Family names
+db(db.Family == 'Aposynaceae', 'Family') = {'Apocynaceae'};
+db(db.Family == 'Araucariacea', 'Family') = {'Araucariaceae'};
+db(db.Family == 'Bombaceae', 'Family') = {'Bombacacea'};
+db(db.Family == 'Bombacacea', 'Family') = {'Bombacaceae'};
+db(db.Family == 'Dipterocapaceae', 'Family') = {'Dipterocarpaceae'};
+db(db.Family == 'Dipterocardaceae', 'Family') = {'Dipterocarpaceae'};
+db(db.Family == 'Dipterucampaceae', 'Family') = {'Dipterocarpaceae'};
+db(db.Family == 'Eupnorbiaceae', 'Family') = {'Euphorbiaceae'};
+db(db.Family == 'Juglandacere', 'Family') = {'Juglandaceae'};
+db(db.Family == 'Lytheraceae', 'Family') = {'Lythraceae'};
+db(db.Family == 'Meliaceaw', 'Family') = {'Meliaceae'};
+db(db.Family == 'Monimoaceae', 'Family') = {'Monimiaceae'};
+db(db.Family == 'Oleaxeae', 'Family') = {'Oleaceae'};
+db(db.Family == 'Pinacee', 'Family') = {'Pinaceae'};
+db(db.Family == 'Simarubrceae', 'Family') = {'Simarubaceae'};
+db(db.Family == 'Taraceae', 'Family') = {'Taxaceae'};
+db(db.Family == 'Olacaceae', 'Family') = {'Oleaceae'};
+db(db.Family == 'Mraxeae' , 'Family') = {'Moraceae'};
+db(db.Family == 'Meliceae' , 'Family') = {'Meliaceae'};
+% Double check that there are no more spelling mistakes. 
+unique(db.Family)
+
+
+writetable(db, 'treeDatabase.csv') % Save database to root
+
+
+data = jsonencode(db);
+
+fid = fopen('myJson.js', 'w')
+fwrite(fid, data, 'char')
+fclose(fid);
--- pages/trunk.js
+++ pages/trunk.js
@@ -1,39 +1,121 @@
-var x = document.getElementsByTagName('body');
-var t = document.getElementById('header');
-var h = t.clientHeight;
+// Javascript
 
-function sortList(choice) {
+//var type = ['L', 'W'];
+var species = [];
+var genus = [];
+var comname = [];
+var family = [];
+var loc = [];
+var collector = [];
+var contributor = [];
+var dates = [];
 
-    var i, j, item, switching, shouldswitch;
-    var tableItem = [];
-    var c = []
-    item = document.getElementsByClassName('item');
-    for (j = 0; j < (item.length); j++) {
-        tableItem.push(item[j].getElementsByClassName('detail'));
-        c.push(tableItem[j][choice].innerHTML);
-    }
-    //console.log(c.sort());
-
-    switching = true;
-    while (switching) {
-        switching = false;
-
-        for (i = 0; i < (item.length - 1); i++) {
-            //console.log(c[i],tableItem[i][choice].innerHTML, c[i+1],tableItem[i + 1][choice].innerHTML);
-            if (c[i] > c[i + 1]) {
-                console.log('should switch')
-                shouldswitch = true;
-                break;
-            }
-
+function generateUniques() {
+    for (var i = 0; i < trees.length; i++) {
+        if (!species.includes(trees[i].Species)) {
+            species.push(trees[i].Species);
         }
-        if (shouldswitch) {
-            tableItem[i][choice].parentNode.parentNode.insertBefore(item[i + 1], item[i]);
-            console.log('Switch', tableItem[i][choice].innerHTML + '->' + tableItem[i + 1][choice].innerHTML);
-            switching = true;
-            console.log(switching);
+        if (!genus.includes(trees[i].Genus)) {
+            genus.push(trees[i].Genus);
+        }
+        if (!comname.includes(trees[i].ComName)) {
+            comname.push(trees[i].ComName);
+        }
+        if (!family.includes(trees[i].Family)) {
+            family.push(trees[i].Family);
+        }
+        if (!loc.includes(trees[i].Location)) {
+            loc.push(trees[i].Location);
+        }
+        if (!collector.includes(trees[i].Collector)) {
+            collector.push(trees[i].Collector);
+        }
+        if (!contributor.includes(trees[i].Contributor)) {
+            contributor.push(trees[i].Contributor);
+        }
+        if (!dates.includes(trees[i].Date)) {
+            dates.push(trees[i].Date);
         }
 
     }
-
+    species.sort();
+    genus.sort();
+    comname.sort();
+    family.sort();
+    loc.sort();
+    collector.sort();
+    contributor.sort();
+    dates.sort();
 }
+
+function updateNav() {
+    nav = document.getElementById('dropdown');
+    console.log(nav.value);
+    switch (nav.value) {
+        case "Species":
+            popNav(species);
+            break;
+        case "Family":
+            popNav(family);
+            break;
+        case "Common Name":
+            popNav(comname);
+            break;
+        case "Genus":
+            popNav(genus);
+            break;
+        case "Location":
+            popNav(loc);
+            break;
+        case "Collector":
+            popNav(collector);
+            break;
+        case "Contributor":
+            popNav(contributor);
+            break;
+        case "Date":
+            popNav(dates);
+            break;
+    }
+}
+
+function popNav(s) {
+    var l = [];
+    for (var i = 0; i < s.length; i++) {
+        l.push('<ni onclick="loadContent(' + "'" + s[i] + "'" + ');">' + s[i] + "</ni>")
+    }
+    var temp = l.join('');
+    return document.getElementById('filter').innerHTML = temp;
+}
+
+function loadContent(c) {
+    var item = [];
+    console.log(c);
+    for (var i = 0; i < trees.length; i++) {
+        if (trees[i].Species == c || trees[i].Family == c || trees[i].Genus == c || trees[i].ComName == c || trees[i].Location == c || trees[i].Collector == c || trees[i].Contributor == c || trees[i].Date == c) {
+            console.log("Match");
+
+            item.push("<div class='item'><div class='picture'><object type='image/jpeg' data ='../images/jpeg/" + trees[i].Num + (trees[i].Type).toLowerCase() + "(a).jpeg'><img src='../images/uoftForestry.jpg'></object></div>");
+            item.push("<table>");
+            item.push("<tr><td class='key'>Type</td><td class='type'>" + trees[i].Type + "</td></tr>");
+            item.push("<tr><td class='key'>Num</td><td class='num'>" + trees[i].Num + "</td></tr>");
+            item.push("<tr><td class='key'>Genus</td><td class='genus'>" + trees[i].Genus + "</td></tr>");
+            item.push("<tr><td class='key'>Species</td><td class='species'>" + trees[i].Species + "</td></tr>");
+            item.push("<tr><td class='key'>Family</td><td class= 'family'>" + trees[i].Family + "</td></tr>")
+            item.push("<tr><td class='key'>ComName</td><td class='comname'>" + trees[i].ComName + "</td></tr>");
+            item.push("<tr><td class='key'>Location</td><td class='location'>" + trees[i].Location + "<td></tr>");
+            item.push("<tr><td class='key'>Collector</td><td class='collector'>" + trees[i].Collector + "</td></tr>");
+            item.push("<tr><td class='key'>Contributor</td><td class='contributor'>" + trees[i].Contributor + "</td></tr>");
+            item.push("<tr><td class ='key'>Date</td><td class='date'>" + trees[i].Date + "</td></tr>");
+            item.push("<tr><td class ='key'>Notes</td><td class ='notes'>" + trees[i].Notes + "</td></tr>");
+            item.push("</table></div>");
+        }
+    }
+    var results = item.join('');
+    return document.getElementById('content').innerHTML = results;
+}
+
+
+generateUniques();
+popNav(species)
+loadContent(species[0]);
